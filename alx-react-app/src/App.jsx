701ff2d97ad0e3bb5a3726b29c3eea2213559ee3
Ondayo <otienosamueloduor@gmail.com>
2025-02-16 22:27:22 +0300

    Modified App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     </div>
   );
 }
+
 import WelcomeMessage from "./components/WelcomeMessage";
 function App() {
   return (
--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
-  return (
-    <Header>
-      <h1>My Favorite Cities!</h1>
-    </Header>
-  );
+    return (
+        <header>
+            <h1>My Favorite Cities</h1>
+        </header>
+    );
 }
 
 export default Header;
